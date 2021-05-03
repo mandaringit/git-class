1c3d2fcb4b4d359824bec00969a199d198555610
mandaringit <mandarin.dev@kakao.com>
2021-05-03 23:44:22 +0900

    change master branch~~

--- abc.js
+++ abc.js
@@ -3,11 +3,3 @@ const b = 7;
 const c = 9;
 const d = 100;
 const e = 10000;
-
-const realWorld = "여긴 실제상황";
-const playground = "놀이터";
-const sand = "놀이터 모래";
-
-const newItem = "놀이터에 입고된 새로운 아이템!";
-
-const rebase = true;
